b80d8cc6a34796e4f928fc96a270d496be44a5c6
Lawrence <Lawrence4code@gmail.com>
2019-11-09 17:52:48 +0530

    Add cors, allow client to access root for testing

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+const cors = require('cors')
 
 // instance of express
 const app = express();
 
+// port
 const port = 8080;
 
+// middlewares
+app.use(cors())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// route
 app.get('/', (req, res) => {
     res.send('root');
 })
-
-
 
 // listening to port
 app.listen(port, () => {
